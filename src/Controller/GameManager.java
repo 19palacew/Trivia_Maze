9c2b62608395f7a1d0f8abd803f613567d2ed148
19palacew <19palacewinston@gmail.com>
2021-11-15 14:19:43 -0800

    Merged SQLite, Movement, and Database Branches Integrated Questions class and Database class.

--- src/Controller/GameManager.java
+++ src/Controller/GameManager.java
@@ -1,7 +1,11 @@
 package Controller;
 
+import Model.Room;
+import Model.RoomBlocker;
+
 public class GameManager {
     public static void main(String[] args) {
-
+        Room rm = new Room(new RoomBlocker(true, true,true, true));
+        System.out.println(rm.getDoor(0).getQuestion());
     }
 }
--- src/Model/Database.java
+++ src/Model/Database.java
@@ -3,11 +3,90 @@ package Model;
 import org.sqlite.SQLiteDataSource;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
+/**
+ * Database class handles connection to the QuestionAnswer table of
+ * the Table database and loading the contents into ArrayLists that
+ * can be accessed via getters
+ */
 public class Database {
+    /**
+     * ArrayList holding the questions
+     */
+    private static final ArrayList<String> questionBank = new ArrayList<>();
+    /**
+     * ArrayList holding the answers
+     */
+    private static final ArrayList<String> answerBank = new ArrayList<>();
+    /**
+     * Counter to iterate through the lists
+     */
+    private static int counter = 0;
+
+    /**
+     * Sets up connection to database and loads table values into ArrayLists
+     */
+    public static void connectToDatabase() {
+        //Create DataSource object
+        SQLiteDataSource ds = new SQLiteDataSource();
+        //Set DataSource URL
+        ds.setUrl("jdbc:sqlite:Table.db");
+        //Set where we want to get our questions and answers from
+        String query = "SELECT * FROM QuestionAnswer";
+        //Set up the connection
+        try ( Connection conn = ds.getConnection();
+              Statement stmt = conn.createStatement(); ) {
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                String question = rs.getString("Question");
+                String answer = rs.getString("Answer");
+
+                questionBank.add(question);
+                answerBank.add(answer);
+            }
+        } catch ( SQLException e ) {
+            e.printStackTrace();
+            System.exit( 0 );
+        }
+    }
+
     public static Question genQuestion(){
-        return new Question("SQLite Coming Soon", Answer.A);
+        return new Question(getQuestion(), getAnswer());
+    }
+
+    /**
+     * Getter for question
+     * @return question at index counter field is set to
+     */
+    public static String getQuestion() {
+        return questionBank.get(counter);
+    }
+
+    /**
+     * Getter for answer
+     * @return answer at index counter field is set to
+     */
+    public static Answer getAnswer() {
+        Answer localAnswer = null;
+        char localDBAns = answerBank.get(counter).charAt(0);
+        switch (localDBAns){
+            case 'A' -> localAnswer = Answer.A;
+            case 'B' -> localAnswer = Answer.B;
+            case 'C' -> localAnswer = Answer.C;
+            case 'D' -> localAnswer = Answer.D;
+        }
+        return localAnswer;
+    }
+
+    /**
+     * Increment counter to get next question
+     */
+    public static void incrementCounter() {
+        counter++;
     }
 }
