1339bd214c97f09a9945ebc346efc83cac6c4781
19palacew <19palacewinston@gmail.com>
2021-11-16 14:06:50 -0800

    Added Instructions Print

--- src/View/Display.java
+++ src/View/Display.java
@@ -4,7 +4,16 @@ public class Display {
     /**
      * Displays the Maze to The User
      */
-    public static void printMaze(){
+    public static void printMaze() {
 
+    }
+
+    public static void printInstructions() {
+        System.out.println("""
+                How to Play:
+                You are in a maze of rooms and you need to escape by finding the door out.
+                To escape a room you need to answer a question correctly to unlock the door.
+                You can move in a direction by typing North, South, East, or West
+                Questions are multiple choice; answer with A, B, C, or D""");
     }
 }
