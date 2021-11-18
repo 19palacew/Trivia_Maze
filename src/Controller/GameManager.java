dc569eed1d56ece635aa41a5c941f976dc8f9b64
19palacew <19palacewinston@gmail.com>
2021-11-18 11:42:36 -0800

    Added start for Gameplay Loop, Rewrote Display to work better with GameManager

--- src/Controller/GameManager.java
+++ src/Controller/GameManager.java
@@ -2,27 +2,84 @@ package Controller;
 
 import Model.Database;
 import Model.Maze;
-import Model.Room;
-import Model.RoomBlocker;
 import View.Display;
 
+import java.util.Scanner;
+
 public class GameManager {
+    private static Display display;
+    private static Maze mainMaze;
+
     public static void main(String[] args) {
         Database.connectToDatabase();
-        Maze mainMaze = new Maze();
         Display.printInstructions();
-        Display.printMaze(mainMaze);
-        Display.printRoom(mainMaze);
-        System.out.println(mainMaze.getCurrentRoom().getDoor(1).getQuestion());
-        mainMaze.setXAndY(0,1);
-        System.out.println(mainMaze.getCurrentRoom().getDoor(0).getQuestion());
-        Display.printMaze(mainMaze);
-        Display.printRoom(mainMaze);
-        mainMaze.setXAndY(1,1);
-        System.out.println(mainMaze.getCurrentRoom().getDoor(0).getQuestion());
+        System.out.println("1) New Game");
+        System.out.println("2) Load Game");
+        Scanner input = new Scanner(System.in);
+        // Load Game / Start New Game
+        boolean gameStarted = false;
+        String userGameStartInput = "";
+        while (!gameStarted) {
+            userGameStartInput = input.nextLine();
+            if (userGameStartInput.equalsIgnoreCase("1")
+                    || userGameStartInput.equalsIgnoreCase("2")
+                    || userGameStartInput.equalsIgnoreCase("quit")) {
+                gameStarted = true;
+            } else {
+                System.out.println("Please Type 1 or 2");
+            }
+        }
+        if (userGameStartInput.equalsIgnoreCase("1")) {
+            newGame();
+        } else if (userGameStartInput.equalsIgnoreCase("2")) {
+            loadGame();
+        } else if (userGameStartInput.equalsIgnoreCase("File")) {
+            File();
+        } else if (userGameStartInput.equalsIgnoreCase("Help")) {
+            Help();
+        }
+        else {
+            //This should never be reached
+            System.out.println("Wait what?");
+        }
+        boolean gameOver = false;
+        while (!gameOver){
+            updateScreen();
+            nextAction();
+            if (!mainMaze.isPossible()){
+                Display.playerLost();
+                gameOver = true;
+            }
+            if (mainMaze.goalReached()){
+                Display.playerWon();
+                gameOver = true;
+            }
+        }
     }
 
-    public static void playGame(){
+    private static void newGame() {
+        mainMaze = new Maze();
+        display = new Display(mainMaze);
+    }
 
+    private static void loadGame() {
+        // Not Yet Implemented
+    }
+
+    private static void nextAction(){
+        // Not Yet Implemented
+    }
+
+    private static void updateScreen(){
+        display.printMaze();
+        display.printRoom();
+    }
+
+    private static void File() {
+        // Not Yet Implemented
+    }
+
+    private static void Help() {
+        // Not Yet Implemented
     }
 }
--- src/View/Display.java
+++ src/View/Display.java
@@ -3,15 +3,19 @@ package View;
 import Model.Maze;
 
 public class Display {
+    private final Maze myMaze;
+    public Display(final Maze theMaze){
+        myMaze = theMaze;
+    }
     /**
      * Displays the Maze to The User
      */
-    public static void printMaze(Maze theMaze) {
-        System.out.println(theMaze.toString());
+    public void printMaze() {
+        System.out.println(myMaze.toString());
     }
 
-    public static void printRoom(Maze theMaze) {
-        System.out.println(theMaze.getCurrentRoom().toString());
+    public void printRoom() {
+        System.out.println(myMaze.getCurrentRoom().toString());
     }
 
     public static void printInstructions() {
@@ -20,6 +24,16 @@ public class Display {
                 You are in a maze of rooms and you need to escape by finding the door out.
                 To escape a room you need to answer a question correctly to unlock the door.
                 You can move in a direction by typing North, South, East, or West
-                Questions are multiple choice; answer with A, B, C, or D""");
+                Questions are multiple choice; answer with A, B, C, or D
+                Type "File" to Save Game, Load Game, Exit or type "Help" to access About,
+                Game Play Instructions, Cheats at anytime""");
+    }
+
+    public static void playerWon() {
+        System.out.println("Goal Reached: Congratulations you won!");
+    }
+
+    public static void playerLost() {
+        System.out.println("No Possible Pathways Left: Game Over");
     }
 }
