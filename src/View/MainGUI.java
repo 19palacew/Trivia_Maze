bf5430f7e254bc4995e5c6c2b85593d951949537
19palacew <19palacewinston@gmail.com>
2021-12-02 08:59:44 -0800

    File menu added

--- src/View/MainGUI.java
+++ src/View/MainGUI.java
@@ -10,21 +10,36 @@ public class MainGUI extends JFrame{
     private JPanel BasePanel;
     private JToolBar ToolBar;
     private JPanel MazeDisplay;
+    private JPopupMenu fileMenu;
+    private JMenuItem menuItemNewGame, menuItemLoadGame, menuItemExitGame;
     private final PlayScreen playScreen;
 
     public MainGUI(final PlayScreen thePlayScreen){
         setTitle("Trivia Maze");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(720, 560);
+        InitializeElements();
+        playScreen = thePlayScreen;
         setContentPane(BasePanel);
         setVisible(true);
-        playScreen = thePlayScreen;
-        fileButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                MazeDisplay.add(playScreen.getBackground());
-                setVisible(true);
-            }
+        fileButton.addActionListener(e -> {
+            ToolBar.add(fileMenu);
+            fileMenu.show(fileMenu, 0, 30);
+            //MazeDisplay.add(playScreen.getBackground());
+            setVisible(true);
         });
+        menuItemNewGame.addActionListener(e -> {
+            System.out.println("asfsf");
+        });
+    }
+
+    private void InitializeElements(){
+        fileMenu = new JPopupMenu();
+        menuItemNewGame = new JMenuItem("New Game");
+        fileMenu.add(menuItemNewGame);
+        menuItemLoadGame = new JMenuItem("Load Game");
+        fileMenu.add(menuItemLoadGame);
+        menuItemExitGame = new JMenuItem("Exit Game");
+        fileMenu.add(menuItemExitGame);
     }
 }
